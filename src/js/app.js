9d52cb5ad2591eae76f24a2559571c2067939afc
DevBielCastro <gabriel.castrogt10@gmail.com>
2025-04-24 13:09:28 -0300

    correção de bugs

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-console.log('App iniciado!');
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Aplicação iniciada!');
-    
-    document.querySelector('.cta-button').addEventListener('click', () => {
-      alert('Formulário de matrícula será aberto!');
-    });
-  });
